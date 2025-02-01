61b48af9533fe3f8b9ab981ab837e6bd9aa2c9c4
basofiibnur <basofiibnur@gmail.com>
2025-02-01 18:00:22 +0700

    feat: fix storybook deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   basePath: '/makyo-dropdown',
-  /* config options here */
+  assetPrefix:
+    process.env.NODE_ENV === 'production' ? '/makyo-dropdown/' : '',
+  images: {
+    loader: 'imgix',
+    path: '',
+  },
 };
 
 export default nextConfig;
